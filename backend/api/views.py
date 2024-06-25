8696a60dbfdb3ae4cb7b5940f4fb9c80c5a91ba6
hunterb2121 <hunterb01@outlook.com>
2024-06-25 09:33:10 -0700

    Updating registration views so that I can make it return API endpoint information instead of returning templates

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password, check_password
 from django.views import View
 from .models import Calendar, Event, SharedCalendar, Permissions
 
@@ -11,6 +12,4 @@ class RegisterView(View):
         return render(request, 'register.html')
 
     def post(self, request):
-        username = request.POST['username']
-        email = request.POST['email']
-        password
+        ...
